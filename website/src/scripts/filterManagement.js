faa6d71655b61e65006351a83e03da28fc0e9b3a
RobertoValente <robertovalennte@gmail.com>
2024-09-29 19:34:21 +0100

    Logic/UI to display ProblemCards + Change Display Type done

--- website/src/scripts/filterManagement.js
+++ website/src/scripts/filterManagement.js
@@ -1,16 +1,28 @@
-window.onload = function() {
+$(document).ready(function() {
     $('#langFilter').change(function() {
         let langSelected = $(this).val();
-        console.log("> " + langSelected);
     });
-
+    
     $('#clearFilter').click(function() {
         if($('#langFilter').val() !== '0') $('#langFilter').val('0').trigger('change');
     });
-
+    
     $('#typeDisplayFormatList').change(function() {
         ($(this).val() === 'list') ? $(this).val('grid') : $(this).val('list');
+
         let newFormat = $(this).val();
-        console.log("> " + newFormat);
+        if(newFormat === 'grid') {
+            $('#content').css({
+                'display': 'grid',
+                'grid-template': 'repeat(2, 1fr) / repeat(2, 1fr)',
+                'gap': '0px 16px'
+            });
+        } else {
+            $('#content').css({
+                'display': '',
+                'grid-template': 'column',
+                'gap': ''
+            });
+        }
     });
-}
+});
