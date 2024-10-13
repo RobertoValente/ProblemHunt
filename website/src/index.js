0f3ebd2392dc7d911c870baaf389a46fa7807ea6
Roberto Valente <robertovalennte@gmail.com>
2024-10-13 18:43:51 +0100

    fix: dont getting .env variables running from source project folder

--- website/src/index.js
+++ website/src/index.js
@@ -1,5 +1,5 @@
 const { NODE_ENV } = require('../../start.js');
-require('dotenv').config({ path: `../.env.${NODE_ENV}` });
+require('dotenv').config({ path: `./.env.${NODE_ENV}` });
 const { sdk, Client, Databases } = require('node-appwrite');
 const express = require('express');
 const app = express();
